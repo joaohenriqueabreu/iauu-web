726db6d57b555b7cf9b4d06f3fbd2c2555e779aa
João Henrique Abreu <joaohenriqueabreu@gmail.com>
2020-05-22 16:04:46 -0300

    Trying to see if axios token is failing on prod

--- plugins/http.js
+++ plugins/http.js
@@ -6,18 +6,18 @@ const http = axios.create({
 
 // TODO include token on requests call - nuxt has server/client and sometimes this gets called by serverside
 //  need to check how to make it work
-if (process.client) {
-  // const token = localStorage.getItem('token')
-  const token = 'o1h4o31yh135pj239p52jpi23j24j0j408u240j2j5025y295y40jy08uy024jy0824u82h4t020'
-  if (token) {
-    http.defaults.headers.authorization = `Bearer ${token}`
-    // http.defaults.headers = {
-    //   host: 'http://localhost:3000',
-    //   accept: '*/*',
-    //   authorization: `Bearer ${token}`
-    // }
-  }
-}
+// if (process.client) {
+// const token = localStorage.getItem('token')
+// const token = 'o1h4o31yh135pj239p52jpi23j24j0j408u240j2j5025y295y40jy08uy024jy0824u82h4t020'
+// if (token) {
+// http.defaults.headers.authorization = `Bearer ${token}`
+// http.defaults.headers = {
+//   host: 'http://localhost:3000',
+//   accept: '*/*',
+//   authorization: `Bearer ${token}`
+// }
+// }
+// }
 
 export default ({ app }, inject) => {
   inject('http', http)
