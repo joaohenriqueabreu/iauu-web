f214f1ed25ea8ec39a16511cd31e58e3e89f9e89
João Henrique Abreu <joaohenriqueabreu@gmail.com>
2020-05-22 15:30:06 -0300

    Trying to fix production

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '@/assets/imgs/logo.png' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     script: [
       {
         src:
